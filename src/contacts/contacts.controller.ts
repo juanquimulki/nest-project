08bc92c6e38b7cac255ff55b84047d800ad3b9b3
Juan Mulki <juanm@humanesources.com>
2022-08-18 08:54:10 -0700

    contact endpoints

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { ContactsService } from './contacts.service';
+import { Prisma, Contact } from '@prisma/client';
 
 @Controller('contacts')
-export class ContactsController {}
+export class ContactsController {
+  constructor(private contactsService: ContactsService) {}
+
+  @Get()
+  async findAll(): Promise<Contact[]> {
+    return this.contactsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Contact> {
+    return this.contactsService.findOne({ id: Number(id) });
+  }
+
+  @Post()
+  async create(@Body() data: Prisma.ContactCreateInput): Promise<Contact> {
+    return this.contactsService.create(data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Contact> {
+    return this.contactsService.delete({ id: Number(id) });
+  }
+}
--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma, Contact } from '@prisma/client';
 
 @Injectable()
-export class ContactsService {}
+export class ContactsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Contact[]> {
+    return this.prisma.contact.findMany();
+  }
+
+  async findOne(id: Prisma.ContactWhereUniqueInput): Promise<Contact | null> {
+    return this.prisma.contact.findUnique({ where: id });
+  }
+
+  async create(data: Prisma.ContactCreateInput): Promise<Contact> {
+    return this.prisma.contact.create({ data });
+  }
+
+  async delete(id: Prisma.ContactWhereUniqueInput): Promise<Contact> {
+    return this.prisma.contact.delete({ where: id });
+  }
+}
