f5f6636f620ac99b4c9b6a5f5d98d5342de8507a
Juan Mulki <juanm@humanesources.com>
2022-08-18 09:16:00 -0700

    fixing tests

--- src/contacts/contacts.service.spec.ts
+++ src/contacts/contacts.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
 import { ContactsService } from './contacts.service';
 
 describe('ContactsService', () => {
@@ -6,7 +7,7 @@ describe('ContactsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContactsService],
+      providers: [ContactsService, PrismaService],
     }).compile();
 
     service = module.get<ContactsService>(ContactsService);
