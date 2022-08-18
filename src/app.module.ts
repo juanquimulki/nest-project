fbb76fa8d9cd219ba17ee473523c421e90bcfdd0
Juan Mulki <juanm@humanesources.com>
2022-08-18 08:20:25 -0700

    prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { CustomersController } from './customers/customers.controller';
 import { ContactsController } from './contacts/contacts.controller';
 import { ContactsService } from './contacts/contacts.service';
 import { CustomersService } from './customers/customers.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CustomersController, ContactsController],
-  providers: [AppService, ContactsService, CustomersService],
+  providers: [AppService, ContactsService, CustomersService, PrismaService],
 })
 export class AppModule {}
