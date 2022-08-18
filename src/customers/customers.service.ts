cdecf472bbe68cbb135af5c909a692b6826d0b02
Juan Mulki <juanm@humanesources.com>
2022-08-18 09:40:54 -0700

    linting

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -11,7 +11,10 @@ export class CustomersService {
   }
 
   async findOne(id: Prisma.CustomerWhereUniqueInput): Promise<Customer | null> {
-    return this.prisma.customer.findUnique({ where: id, include: { contacts: true } });
+    return this.prisma.customer.findUnique({
+      where: id,
+      include: { contacts: true },
+    });
   }
 
   async create(data: Prisma.CustomerCreateInput): Promise<Customer> {
