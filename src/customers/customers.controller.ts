a1a02893d4e9d3313b57855b2194e83c7b8a9721
Juan Mulki <juanm@humanesources.com>
2022-08-18 08:23:10 -0700

    customer endpoints

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { CustomersService } from './customers.service';
+import { Prisma, Customer } from '@prisma/client';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private customersService: CustomersService) {}
+
+  @Get()
+  async findAll(): Promise<Customer[]> {
+    return this.customersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Customer> {
+    return this.customersService.findOne({ id: Number(id) });
+  }
+
+  @Post()
+  async create(@Body() data: Prisma.CustomerCreateInput): Promise<Customer> {
+    return this.customersService.create(data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<Customer> {
+    return this.customersService.delete({ id: Number(id) });
+  }
+}
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma, Customer } from '@prisma/client';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Customer[]> {
+    return this.prisma.customer.findMany();
+  }
+
+  async findOne(id: Prisma.CustomerWhereUniqueInput): Promise<Customer | null> {
+    return this.prisma.customer.findUnique({ where: id, include: { contacts: true } });
+  }
+
+  async create(data: Prisma.CustomerCreateInput): Promise<Customer> {
+    return this.prisma.customer.create({ data });
+  }
+
+  async delete(id: Prisma.CustomerWhereUniqueInput): Promise<Customer> {
+    return this.prisma.customer.delete({ where: id });
+  }
+}
